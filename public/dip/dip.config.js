87f0d408595de034e4d6b78881786f8f863b75bf
Glitch (infrequent-wave-journey) <none>
2022-10-13 01:28:32 +0000

    🎩🏏 Checkpoint ./public/dip/dip.config.js:38683812/121

--- public/dip/dip.config.js
+++ public/dip/dip.config.js
@@ -11,8 +11,8 @@ self.__DIP.config = {
     path: '/bare/',
   },
   tab: {
-    title: 'Dynamic Interception Proxy',
-    icon: 'https://google.com/favicon.ico',
+    title: 'Dynamic-Interception-Proxy',
+    icon: 'https://cdn.glitch.global/08b4ac66-1815-482b-a7b8-ed25560d4aa1/dip.jpeg?v=1665611076534',
     ua: 'Mozilla/5.0 (X11; CrOS x86_64 14388.61.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.107 Safari/537.36'
   }
 };
